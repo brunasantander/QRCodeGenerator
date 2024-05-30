ffd28f26a49ad82b3a05d89677aa4bda8501259b
Bruna Santander <brunasantantader@gmail.com>
2024-05-30 16:22:53 -0300

    fix: save button

--- js/script.js
+++ js/script.js
@@ -1,86 +1,84 @@
-const form = document.getElementById("generate-form");
-const qr = document.getElementById("qrcode");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("generate-form");
+  const qr = document.getElementById("qrcode");
 
-// Button submit
-const onGenerateSubmit = (e) => {
-  e.preventDefault();
+  // Button submit
+  const onGenerateSubmit = (e) => {
+    e.preventDefault();
 
-  clearUI();
+    clearUI();
 
-  const url = document.getElementById("url").value;
-  const size = document.getElementById("size").value;
+    const url = document.getElementById("url").value;
+    const size = document.getElementById("size").value;
 
-  // Validate url
-  if (url === "") {
-    alert("Please enter a URL");
-  } else {
-    showSpinner();
-    // Show spinner for 1 sec
-    setTimeout(() => {
-      hideSpinner();
-      generateQRCode(url, size);
-      showScanner();
-      // Generate the save button after the qr code image src is ready
+    // Validate url
+    if (url === "") {
+      alert("Please enter a URL");
+    } else {
+      showSpinner();
+      // Show spinner for 1 sec
       setTimeout(() => {
-        // Get save url
-        const saveUrl = qr.querySelector("canvas").toDataURL();
-        // Create save button
-        createSaveBtn(saveUrl);
-      }, 50);
-    }, 1000);
-  }
-};
+        hideSpinner();
+        generateQRCode(url, size);
+        // Generate the save button after the qr code image src is ready
+        setTimeout(() => {
+          // Get save url
+          const canvas = qr.querySelector("canvas");
+          if (canvas) {
+            const saveUrl = canvas.toDataURL();
+            // Create save button
+            createSaveBtn(saveUrl);
+          }
+        }, 50);
+      }, 1000);
+    }
+  };
 
-// Generate QR code
-const generateQRCode = (url, size) => {
-  const qrcode = new QRCode("qrcode", {
-    text: url,
-    width: size,
-    height: size,
-  });
-};
+  // Generate QR code
+  const generateQRCode = (url, size) => {
+    new QRCode(qr, {
+      text: url,
+      width: size,
+      height: size,
+    });
+  };
 
-// Clear QR code and save button
-const clearUI = () => {
-  qr.innerHTML = "";
-  const saveBtn = document.getElementById("save-link");
-  if (saveBtn) {
-    saveBtn.remove();
-  }
-};
+  // Clear QR code and save button
+  const clearUI = () => {
+    qr.innerHTML = "";
+    const saveBtn = document.getElementById("save-link");
+    if (saveBtn) {
+      saveBtn.remove();
+    }
+  };
 
-// hide  scanner
-const showScanner = () => {
-  const scanner = document.getElementById("qrCodeContainer");
-  scanner.style.display = "block";
-};
+  // Show spinner
+  const showSpinner = () => {
+    const spinner = document.getElementById("spinner");
+    spinner.style.display = "block";
+  };
 
-// Show spinner
-const showSpinner = () => {
-  const spinner = document.getElementById("spinner");
-  spinner.style.display = "block";
-};
+  // Hide spinner
+  const hideSpinner = () => {
+    const spinner = document.getElementById("spinner");
+    spinner.style.display = "none";
+  };
 
-// Hide spinner
-const hideSpinner = () => {
-  const spinner = document.getElementById("spinner");
-  spinner.style.display = "none";
-};
+  // Create save button to download QR code as image
+  const createSaveBtn = (saveUrl) => {
+    const link = document.createElement("a");
+    link.id = "save-link";
+    link.classList =
+      "bg-customBlue hover:bg-customBlue text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+    link.innerHTML = "Save Image";
 
-// Create save button to download QR code as image
-const createSaveBtn = (saveUrl) => {
-  const link = document.createElement("a");
-  link.id = "save-link";
-  link.classList =
-    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
-  link.innerHTML = "Save Image";
+    link.href = saveUrl;
+    link.download = "qrcode.png";
 
-  link.href = saveUrl;
-  link.download = "qrcode.png";
+    document.getElementById("generated").appendChild(link);
+  };
 
-  document.getElementById("generated").appendChild(link);
-};
+  hideSpinner();
 
-hideSpinner();
-
-form.addEventListener("submit", onGenerateSubmit);
+  form.addEventListener("submit", onGenerateSubmit);
+});
